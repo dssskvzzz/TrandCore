e2b69dcca44c9858cde4a2a07f06ee047c22d862
aquiya <96477828+dssskvzzz@users.noreply.github.com>
2024-12-04 01:43:37 +0200

    Auto-commit: 2024-12-03T23:43:37.134Z

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
 
 interface HeaderProps {
   className?: string;
@@ -6,31 +7,13 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ className = '' }) => {
   return (
-    <header className={`bg-neutral-950 w-full h-16 border-b-2 border-neutral-900${className}`}>
+    <header className={`bg-neutral-950 w-full h-16 ${className}`}>
       <div className="flex items-center justify-between px-4 h-full">
         <h1 className="text-white text-lg font-semibold">MyWebsite</h1>
         <nav>
           <ul className="flex space-x-4">
-            <li>
-              <a href="#" className="text-white hover:text-gray-400">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-white hover:text-gray-400">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-white hover:text-gray-400">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-white hover:text-gray-400">
-                Contact
-              </a>
-            </li>
+            <Button>Sing Up</Button>
+            <Button variant={"ghost"}>Log In</Button>
           </ul>
         </nav>
       </div>
