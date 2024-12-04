9393350e627113e756c8fa27e34c79366e6d861b
aquiya <96477828+dssskvzzz@users.noreply.github.com>
2024-12-04 02:06:13 +0200

    Auto-commit: 2024-12-04T00:06:13.502Z

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import { NavMenu } from '@/components/NavMenu';
 
 interface HeaderProps {
   className?: string;
@@ -9,13 +11,18 @@ const Header: React.FC<HeaderProps> = ({ className = '' }) => {
   return (
     <header className={`bg-neutral-950 w-full h-16 ${className}`}>
       <div className="flex items-center justify-between px-4 h-full">
-        <h1 className="text-white text-lg font-semibold">MyWebsite</h1>
-        <nav>
+        <div>
+          <Image src="/icon.png" alt="Logo" width={130} height={32} />
+        </div>
+        <div>
+          <NavMenu />
+        </div>
+        <div>
           <ul className="flex space-x-4">
-            <Button>Sing Up</Button>
-            <Button variant={"ghost"}>Log In</Button>
+            <Button className='font-semibold'>Sing Up</Button>
+            <Button variant={"ghost"} className='font-semibold'>Log In</Button>
           </ul>
-        </nav>
+        </div>
       </div>
     </header>
   );
