2c7914627ea4e9240ceec8de69cadadc936e466f
aquiya <96477828+dssskvzzz@users.noreply.github.com>
2024-12-06 02:52:43 +0200

    Auto-commit: 2024-12-06T00:52:43.497Z

--- components/MainMenuPanel.tsx
+++ components/MainMenuPanel.tsx
@@ -1,20 +1,21 @@
-import React, { ReactNode } from 'react';
+import React from 'react';  // Removed ReactNode import since it's not being used
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+
 interface MainMenuProps {
     className?: string;
 }
 
 const MainMenu: React.FC<MainMenuProps> = ({ className = '' }) => {
     return (
-        <div className="flex GAP-4">
+        <div className={`flex gap-4 ${className}`}> {/* Used className if it's passed */}
             <div className="w-1/2 ml-80 mt-72">
                 <h1 className="text-5xl font-bold mb-4">
-                    The world’s <br /> most customizable <br /> crypto trading bot
+                    The world&apos;s <br /> most customizable <br /> crypto trading bot
                 </h1>
                 <p className="text-neutral-400 mt-6">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum <br />
-                    has been the industry's standard dummy text ever since the 1500s, when an unknown printer <br />
+                    has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer <br />
                     took a galley of type and scrambled it to make a type specimen book.
                 </p>
                 <Button className="mt-6">Start Free Trial</Button>
