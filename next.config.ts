fa311e7c9bfb7b66182fb9a57d17b34953a508f6
aquiya <96477828+dssskvzzz@users.noreply.github.com>
2024-12-08 05:53:14 +0200

    Auto-commit: 2024-12-08T03:53:14.658Z

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /prisma\/.*\.mjs$/,
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
